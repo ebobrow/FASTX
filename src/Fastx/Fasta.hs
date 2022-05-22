43a14e96641948a9dd48aaf488e267ce0313a2b5
Elliot Bobrow <elliotgreybobrow@gmail.com>
2022-05-22 15:26:48 -0700

    simple FASTA parser

--- src/Fastx/Fasta.hs
+++ src/Fastx/Fasta.hs
@@ -1,4 +1,41 @@
-module Fastx.Fasta (someFunc) where
+module Fastx.Fasta (
+    parseOne,
+    parseMany,
+    parseFile,
+    Sequence (..),
+) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Functor
+import Text.ParserCombinators.Parsec
+
+data Sequence = Sequence {description :: String, sqn :: String} deriving (Show, Eq)
+
+nucleicAcid :: Parser Char
+nucleicAcid = oneOf "ACGTUIRYKMSWBDHVN-"
+
+aminoAcid :: Parser Char
+aminoAcid = oneOf "ABCDEFGHIJKLMNOPQRSTUVWYZX*-"
+
+-- TODO: map toUpper
+parseLine :: Parser String
+parseLine = do
+    line <- many (nucleicAcid <|> aminoAcid)
+    newline
+    return line
+
+parseSequence :: Parser Sequence
+parseSequence = do
+    char '>'
+    description <- many $ noneOf "\n"
+    char '\n'
+    sqn <- many parseLine
+    return $ Sequence description (concat sqn)
+
+parseOne :: String -> Either ParseError Sequence
+parseOne = parse parseSequence ""
+
+parseMany :: String -> Either ParseError [Sequence]
+parseMany = parse (many parseSequence) ""
+
+parseFile :: FilePath -> IO (Either ParseError [Sequence])
+parseFile = parseFromFile $ many parseSequence
---
+++ tests/FastaTest.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import Fastx.Fasta
+import Test.Tasty
+import Test.Tasty.HUnit
+
+main :: IO ()
+main = defaultMain parseTests
+
+-- TODO: allow no trailing newline
+oneSeq :: String
+oneSeq =
+    ">Description\n\
+    \ABC\n\
+    \DEF\n"
+
+manySeqs :: String
+manySeqs =
+    ">D1\n\
+    \AGCT\n\n\
+    \>D2\n\
+    \HE\n\
+    \LLO\n"
+
+parseTests :: TestTree
+parseTests =
+    testGroup
+        "FASTA Parsing Tests"
+        [ testCase "Read One" $
+            parseOne oneSeq @?= Right (Sequence "Description" "ABCDEF")
+        , testCase "Read Many" $
+            parseMany manySeqs @?= Right [Sequence "D1" "AGCT", Sequence "D2" "HELLO"]
+        , testCase "From File" $
+            parseFile
+                "tests/test.fasta"
+                >>= (@=? Right [Sequence "Amino acids" "ABCDEF", Sequence "Nucleic acids" "ACGT"])
+        ]
